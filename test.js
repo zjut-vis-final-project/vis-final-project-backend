b3c86e5f807241b0062a6dfc3fb41e5e02337399
蔡鎔玖 <874088962@qq.com>
2020-06-14 16:05:35 +0800

    update rumors

--- test.js
+++ test.js
@@ -1,24 +1,5 @@
-var datadata = require('./public/data/DXYArea-TimeSeries.json');
-var fs = require('fs');
-var _world = require('./public/data/world_countries.json');
-var data = new Object(datadata).results;
-var world = new Object(_world).features;
-// var results = {};
-// data.forEach(function (d, i) {
-//   if (results[d.countryEnglishName]) {
-//     results[d.countryEnglishName] += d.confirmedCount;
-//   } else {
-//     results[d.countryEnglishName] = d.confirmedCount;
-//   }
-// });
-// console.log(world.map((item) => item.id));
-var results = world.map((item) => item.id);
-// console.log(new Date(1584271456374));
-
-fs.writeFile('world_country_ids.json', JSON.stringify(results), function (err) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('done');
-  }
+var data_origin = require('./public/data/DXYRumors-TimeSeries.json');
+var data_s = data_origin.sort((a, b) => {
+  a.crawlTime < b.crawlTime ? 1 : -1;
 });
+console.log(data_s);
